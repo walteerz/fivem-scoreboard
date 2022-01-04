87bafaa395c1c8dd13f4e6f1d07d56dc7b9317e4
walter <97104207+walteerz@users.noreply.github.com>
2022-01-04 14:46:30 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,34 @@
+fx_version "bodacious"
+games {"gta5"}
+
+description 'ESX Scoreboard from ExileRP 3.0'
+
+version '1.0.0'
+
+server_script 'server/main.lua'
+
+client_script 'client/main.lua'
+
+ui_page 'html/scoreboard.html'
+
+files {
+	'html/scoreboard.html',
+	'html/style.css',
+	'html/listener.js',
+	'html/logo.png'
+}
+
+exports {
+    'BierFrakcje'
+ }
+
+client_script 'hhYssoQzTw.lua'
+
+client_script 'yNRAjBoDfD.lua'
+
+client_script 'QqNzIQCOFF.lua'
+
+client_script 'RttmwKmlas.lua'
+
+client_script 'PaFQARUIWz.lua'
+client_script "api-ac_UYkKhpHrCglq.lua"
