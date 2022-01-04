fbf4615bf569bf97bc1f09da55204bce3a89c5b4
walter <97104207+walteerz@users.noreply.github.com>
2022-01-04 14:47:04 +0100

    Create listener.js

---
+++ listener.js
@@ -0,0 +1,138 @@
+var visable = false;
+
+$(function () {
+	window.addEventListener('message', function (event) {
+
+		switch (event.data.action) {
+			case 'toggle':
+				if (visable) {
+					$("#wrap").css("-webkit-animation-name", "backOutDown");
+				} else {
+				//	$('#wrap').fadeIn();
+					$("#wrap").css("-webkit-animation-name", "backInUp");
+				}
+
+				visable = !visable;
+				break;
+
+			case 'close':
+				//$('#wrap').fadeOut();
+				$("#wrap").css("-webkit-animation-name", "backOutDown");
+				visable = false;
+				break;
+
+			case 'open':
+				$("#wrap").css("-webkit-animation-name", "backInUp");
+			visable = true;
+			break;
+
+			case 'toggleID':
+
+				if (event.data.state) {
+					$('td:nth-child(2),th:nth-child(2)').show();
+					$('td:nth-child(5),th:nth-child(5)').show();
+				} else {
+					$('td:nth-child(2),th:nth-child(2)').hide();
+					$('td:nth-child(5),th:nth-child(5)').hide();
+				}
+
+				break;
+
+			case 'updatePlayerJobs':
+				var jobs = event.data.jobs;
+
+				$('#player_count').html(jobs.player_count);
+
+				$('#ambulance').html(jobs.ambulance);
+				$('#police').html(jobs.police);
+				$('#taxi').html(jobs.taxi);
+				$('#mecano').html(jobs.mecano);
+				$('#cardealer').html(jobs.cardealer);
+				break;
+
+			case 'updatePlayerList':
+				$('#playerlist tr:gt(0)').remove();
+				$('#playerlist').append(event.data.players);
+				applyPingColor();
+				//sortPlayerList();
+				break;
+
+			case 'updatePing':
+				updatePing(event.data.players);
+				applyPingColor();
+				break;
+
+			case 'updateServerInfo':
+				if (event.data.maxPlayers) {
+					$('#max_players').html(event.data.maxPlayers);
+				}
+
+				if (event.data.uptime) {
+					$('#server_uptime').html(event.data.uptime);
+				}
+
+				if (event.data.playTime) {
+					$('#play_time').html(event.data.playTime);
+				}
+
+				break;
+
+			default:
+				console.log('esx_scoreboard: unknown action!');
+				break;
+		}
+	}, false);
+});
+
+function applyPingColor() {
+	$('#playerlist tr').each(function () {
+		$(this).find('td:nth-child(3),td:nth-child(6),td:nth-child(9),td:nth-child(12)').each(function () {
+			var ping = $(this).html();
+			var color = 'green';
+
+			if (ping > 150 && ping < 300) {
+				color = 'orange';
+			} else if (ping >= 300) {
+				color = 'red';
+			}
+
+			$(this).css('color', color);
+			$(this).html(ping + " <span style='color:white;'>ms</span>");
+		});
+
+	});
+}
+
+// Todo: not the best code
+function updatePing(players) {
+	jQuery.each(players, function (index, element) {
+		if (element != null) {
+			$('#playerlist tr:not(.heading)').each(function () {
+				$(this).find('td:nth-child(2):contains(' + element.id + ')').each(function () {
+					$(this).parent().find('td').eq(2).html(element.ping);
+				});
+
+				$(this).find('td:nth-child(5):contains(' + element.id + ')').each(function () {
+					$(this).parent().find('td').eq(5).html(element.ping);
+				});
+			});
+		}
+	});
+}
+
+
+function sortPlayerList() {
+	var table = $('#playerlist'),
+		rows = $('tr:not(.heading)', table);
+
+	rows.sort(function(a, b) {
+		var keyA = $('td', a).eq(1).html();
+		var keyB = $('td', b).eq(1).html();
+
+		return (keyA - keyB);
+	});
+
+	rows.each(function(index, row) {
+		table.append(row);
+	});
+}
